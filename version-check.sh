d85d7ad47f4645d6b2bbc4b29f35eed76592460b
ramz <saikrn112@gmail.com>
2018-10-17 17:32:20 +0530

    added compilation

--- version-check.sh
+++ version-check.sh
@@ -41,3 +41,9 @@ sed --version | head -n1
 tar --version | head -n1
 makeinfo --version | head -n1
 xz --version | head -n1
+
+echo 'int main(){}' > dummy.c && g++ -o dummy dummy.c
+if [ -x dummy ]
+then echo "g++ compilation OK";
+else echo "g++ compilation failed"; fi
+rm -f dummy.c dummy
